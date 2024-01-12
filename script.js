1206f97779d1f2fc8af48ccf91e31c07880da5c3
Punam-Rani <149293663+Punam-Rani@users.noreply.github.com>
2024-01-12 20:19:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var divv=document.getElementById("myDiv");
+
+divv.style.color="blue";
+divv.style.backgroundColor="yellow";
+divv.style.margin="20px";
+divv.style.padding="10px";
+divv.style.fontSize="18px";
+divv.style.fontWeight="bold";
+divv.style.height="200px";
+divv.style.width="300px";
